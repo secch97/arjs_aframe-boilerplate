a3f34e9ade661aee5fd3b580ef7ab7732bdb1097
miminiverse <minhtamphamtol@gmail.com>
2024-03-13 20:17:23 -0400

    add aframe-react types

---
+++ types/aframe-react.d.ts
@@ -0,0 +1,9 @@
+declare module "aframe-react" {
+  import { Component } from "react";
+
+  // Define the Entity component
+  export const Entity: Component;
+
+  // Define the Scene component
+  export const Scene: Component;
+}

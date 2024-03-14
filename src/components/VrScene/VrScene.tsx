1bdace216a22ee20fe630433e6eea66f06b1eeb6
mimi <minhtamphamtol@gmail.com>
2024-03-14 10:49:09 -0400

    add table

--- src/components/VrScene/VrScene.tsx
+++ src/components/VrScene/VrScene.tsx
@@ -6,11 +6,11 @@ import { Entity, Scene } from "aframe-react";
 const VrScene = () => {
   return (
     <Scene vr-mode-ui="enabled: false">
-      <a-assets>
-        <a-asset-item id="bulbasaur" src="/bulbasaur.glb"></a-asset-item>
-      </a-assets>
+      {/* <a-assets>
+        <a-asset-item id="bulbasaur" src="/table/bulbasaur.glb"></a-asset-item>
+      </a-assets> */}
       <Entity
-        gltf-model="#bulbasaur"
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
         scale={{ x: 0.75, y: 0.75, z: 0.75 }}
         position={{ x: 0, y: 0, z: -5 }}
       />

04c0b28a9339686c166f9015a4bb5cd983ef1012
mimi <minhtamphamtol@gmail.com>
2024-03-14 11:37:35 -0400

    add renderer

--- src/components/VrScene/VrScene.tsx
+++ src/components/VrScene/VrScene.tsx
@@ -5,11 +5,13 @@ import { Entity, Scene } from "aframe-react";
 
 const VrScene = () => {
   return (
-    <Scene>
+    <Scene
+    renderer="antialias: true; logarithmicDepthBuffer: true; colorManagement: true; sortObjects: true;"
+    >
               <Entity
           id="sphere" geometry="primitive: sphere"
           material="color: #EFEFEF; shader: flat"
-          position="0 0 0"
+          position="0 0 -3"
           light="type: point; intensity: 5"
           animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: 0 -0.10 -5; loop: true"
         ></Entity>

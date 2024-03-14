6e497cba853587fc5a6abd352ec57bd3f8264b51
Saul Castillo <secch97@gmail.com>
2024-03-14 16:21:26 -0400

    1. Updated position of aplane

--- src/components/VrScene/VrScene.tsx
+++ src/components/VrScene/VrScene.tsx
@@ -7,7 +7,7 @@ const VrScene = () => {
       <a-marker type="pattern" url="/pattern-marker.patt">
         <a-plane
           id="imagePlane"
-          position="0 0 0"
+          position="0 -1 0"
           rotation="-90 0 0"
           width="4"
           height="2.25"

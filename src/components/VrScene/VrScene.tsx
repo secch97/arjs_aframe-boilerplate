64859c1bd654a589f2e6889d882330e8a7dcb545
mimi <minhtamphamtol@gmail.com>
2024-03-14 12:12:23 -0400

    add more tables

--- src/components/VrScene/VrScene.tsx
+++ src/components/VrScene/VrScene.tsx
@@ -11,16 +11,37 @@ const VrScene = () => {
     embedded
     arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
     >
-              <Entity
-          id="sphere" geometry="primitive: sphere"
-          material="color: #EFEFEF; shader: flat"
-          position="0 0 -3"
-          light="type: point; intensity: 5"
-          animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: 0 -0.10 -5; loop: true"
-        ></Entity>
       <Entity
         gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
         position="2 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="1 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="3 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="4 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="5 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="6 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="7 0 -5"
+      />
+          <Entity
+        gltf-model="https://arjs-aframe-boilerplate.vercel.app/table/scene.gltf"
+        position="8 0 -5"
       />
         <Entity primitive="a-camera">
           {/* <Entity primitive="a-cursor" animation__click={{ property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150 }} /> */}

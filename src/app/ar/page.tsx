2f72031b418f6b9b7066c9a64b3ec2fc876b2b26
Saul Castillo <secch97@gmail.com>
2024-03-13 20:05:30 -0400

    1. Package fixes

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -1,30 +1,24 @@
-"use client"
+"use client";
 import "aframe";
-import 'ar.js';
+import "ar.js";
 import { Entity, Scene } from "aframe-react";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function ARpage() {
-
-    return (
-        <>
-            <Scene vr-mode-ui="enabled: false">
-                <a-assets>
-                    <a-asset-item
-                        id="bulbasaur"
-                        src="/bulbasaur.glb"
-                    ></a-asset-item>
-                </a-assets>
-                <Entity
-                    gltf-model="#bulbasaur"
-                    scale={{ x: 0.75, y: 0.75, z: 0.75 }}
-                    position={{ x: 0, y: 0, z: -5 }}
-                />
-            </Scene>
-
-        </>
-
-    );
+  return (
+    <>
+      <Scene vr-mode-ui="enabled: false">
+        <a-assets>
+          <a-asset-item id="bulbasaur" src="/bulbasaur.glb"></a-asset-item>
+        </a-assets>
+        <Entity
+          gltf-model="#bulbasaur"
+          scale={{ x: 0.75, y: 0.75, z: 0.75 }}
+          position={{ x: 0, y: 0, z: -5 }}
+        />
+      </Scene>
+    </>
+  );
 }
 
 export default ARpage;

dedbdcfa10ac0bb91ebcb5498d06c960e9045f94
mimi <minhtamphamtol@gmail.com>
2024-03-14 21:02:50 -0400

    test ios

--- src/components/VrScene/VrScene.tsx
+++ src/components/VrScene/VrScene.tsx
@@ -7,9 +7,11 @@ const VrScene = () => {
         <a-assets>
     <a-asset-item id="table" src="/table/scene.gltf"></a-asset-item>
   </a-assets>
-      <a-marker type="pattern" url="/pattern-marker.patt">
-      <a-gltf-model src="#table"></a-gltf-model>
-      </a-marker>
+  <a-box src="#boxTexture" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
+
+      {/* <a-marker type="pattern" url="/pattern-marker.patt">
+      </a-marker> */}
+      <a-gltf-model scale="0.5 0.5 0.5" src="#table"></a-gltf-model>
       <a-entity camera></a-entity>
     </a-scene>
   );
